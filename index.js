1c009b6d65b8a0c5919a59046b384e8f7ba2fe6e
Sindre Sorhus <sindresorhus@gmail.com>
2014-08-01 15:04:14 +0200

    remove faulty assumption of npm not being used to start a server on Heroku
    I thought Procfile were used, but apparently not always.
    
    fixes #3

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 'use strict';
-var isNpm = require('is-npm');
-
-module.exports = 'HEROKU' in process.env ||
-	(!isNpm && 'DYNO' in process.env && process.env.HOME === '/app');
+module.exports = 'HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/app');
